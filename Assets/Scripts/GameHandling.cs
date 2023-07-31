f294bf31b0d747f55fd3e7e2dd32add79c20eba7
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2023-07-31 14:30:51 +0800

    Made changes to the GameHandler and GameScene.

--- Assets/Scripts/GameHandling.cs
+++ Assets/Scripts/GameHandling.cs
@@ -8,36 +8,15 @@ public class GameHandling : MonoBehaviour
 {
     [SerializeField] private Image blackout;
 
-    [SerializeField] private Image speechBubbleStem;
-    [SerializeField] private Text speechBubble;
-    // [SerializeField] private Text dialogueOption1;
-    // [SerializeField] private Text dialogueOption2;
-
     [SerializeField] private GameObject button1;
     [SerializeField] private GameObject button2;
 
-    // private Text bubbleText;
-    // filepath of text
-    //private string[] filePath;
-    //private int lineIndex = 0;
-    //private int dialogueID = 0;
-
     private bool isFadingIn = false;
     private bool isFadingOut = false;
-    private bool isTalking = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        for (int i = 0; i < 12; i++)
-        {
-            filePath[i] = Application.dataPath + "/Text/Speech" + (i + 1) + ".txt";
-        }
-        */
-        speechBubble = GameObject.Find("Speech Bubble").GetComponent<Text>();
-        Debug.Log("Found gameObject with name " + speechBubble.gameObject.name);
-        //speechBubble.gameObject.SetActive(false);
         FadeIn();
     }
 
@@ -53,9 +32,6 @@ public class GameHandling : MonoBehaviour
             {
                 blackout.gameObject.SetActive(false);
                 isFadingIn = false;
-                //speechBubbleStem.gameObject.SetActive(true);
-                speechBubble.gameObject.SetActive(true);
-                isTalking = true;
             }
         }
 
@@ -69,20 +45,6 @@ public class GameHandling : MonoBehaviour
                 isFadingOut = false;
             }
         }
-
-        /*if (isTalking)
-        {
-            Color currentColor = speechBubbleStem.color;
-            currentColor.a += Time.deltaTime;
-            speechBubbleStem.color = currentColor;
-            //speechBubble1.color = currentColor;
-            if (currentColor.a >= 1.0f)
-            {
-                isTalking = false;
-                button1.SetActive(true);
-                button2.SetActive(true);
-            }
-        }*/
     }
 
     public void ChangeDialogue(bool isOption1)
